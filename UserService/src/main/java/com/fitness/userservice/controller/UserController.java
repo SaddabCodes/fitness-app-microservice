d956334f72f69a75a2c013bc1cc0eb8015bc8af2
Saddab <saddabakhtar17@gmail.com>
2025-08-27 23:58:50 +0530

    add getUserProfile

--- UserService/src/main/java/com/fitness/userservice/controller/UserController.java
+++ UserService/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -4,11 +4,9 @@ import com.fitness.userservice.dto.RegisterRequest;
 import com.fitness.userservice.dto.UserResponse;
 import com.fitness.userservice.services.UserService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,12 +14,17 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse>register(@Valid @RequestBody RegisterRequest registerRequest){
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
         return ResponseEntity.ok(userService.register(registerRequest));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId) {
+        return new ResponseEntity<>(userService.getUserProfile(userId), HttpStatus.OK);
     }
 }
--- UserService/src/main/java/com/fitness/userservice/services/UserService.java
+++ UserService/src/main/java/com/fitness/userservice/services/UserService.java
@@ -40,4 +40,21 @@ public class UserService {
 
         return userResponse;
     }
+
+    public UserResponse getUserProfile(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        UserResponse userResponse = new UserResponse();
+
+        userResponse.setId(user.getId());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+
+        return userResponse;
+    }
 }
